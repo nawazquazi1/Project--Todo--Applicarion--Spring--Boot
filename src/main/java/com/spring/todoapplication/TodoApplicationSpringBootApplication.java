cbcda8e167568a2d311b8b51f500d522f15c4234
nawaz <nawazquazi356@gmail.com>
2023-09-15 23:16:30 +0530

    Project On Progress

---
+++ src/main/java/com/spring/todoapplication/TodoApplicationSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.spring.todoapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoApplicationSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoApplicationSpringBootApplication.class, args);
+    }
+
+}
